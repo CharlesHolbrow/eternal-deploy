7224a926a1a4d0281a362dc127ad6d621b10924b
Charles Holbrow <charlesholbrow@gmail.com>
2017-09-19 19:58:24 -0400

    eternal-action gets redis host from env var

--- gopath/src/github.com/CharlesHolbrow/eternal-action/main.go
+++ gopath/src/github.com/CharlesHolbrow/eternal-action/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/CharlesHolbrow/eternal"
 	"github.com/CharlesHolbrow/synk"
 )
 
+// By default, look for redis locally at ":6379". We may specify another host by
+// setting the "SYNK_REDIS_HOST" environment variable.
+var redisAddr = os.Getenv("SYNK_REDIS_HOST") + ":6379"
+
+// SYNK_ENV should be "production" or an empty string. We may add additional
+// environments in the future. For now, if this is an empty string, or if it is
+// not set, assume development.
+var env = os.Getenv("SYNK_ENV")
+
 func main() {
-	synkConn := synk.NewConnection(":6379")
+	synkConn := synk.NewConnection(redisAddr)
 	subKey := "eternal:main"
 
 	part := eternal.NewFragment(subKey, synkConn)
