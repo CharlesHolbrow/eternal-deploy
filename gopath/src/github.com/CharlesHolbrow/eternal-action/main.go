841ac6b4ed6f47677ef9beacc303d6f528dab5b4
Charles Holbrow <charlesholbrow@gmail.com>
2017-10-09 17:00:38 -0400

    eternal-action no longer initializes data

--- gopath/src/github.com/CharlesHolbrow/eternal-action/main.go
+++ gopath/src/github.com/CharlesHolbrow/eternal-action/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/CharlesHolbrow/eternal"
@@ -27,40 +26,6 @@ func main() {
 	subKey := "eternal|main"
 
 	part := eternal.NewFragment(subKey, synkConn)
-	time.Sleep(30 * time.Millisecond)
-
-	// If there are few voices, add one
-	if len(part.Voices) < 3 {
-		voice := &eternal.Voice{
-			SubKey: subKey,
-		}
-		voice.SetLinksElement(0, "n:eternal|main")
-		length := 4 // how many beats long
-		for i := 0; i < length; i++ {
-			voice.Lengths[i] = 1
-		}
-		conn := synkConn.Pool.Get()
-		synk.Create(voice, conn)
-		conn.Close()
-		part = eternal.NewFragment(subKey, synkConn)
-	}
-
-	// This app expects there to be a single 'root object' where users begin
-	// their journey.
-	neil := "Neil deGrasse Tyson"
-	initial := &eternal.Note{
-		Text:   "If you want to assert a truth, first make sure it's not just an opinion that you desperately want to be true.",
-		Attrib: &neil,
-		// Note that IDs may not contain a colon character, because this will
-		// interfere with the object loader's type identification.
-		ID: strings.Replace(subKey, ":", "|", -1),
-	}
-	// Create the root object in Redis if it was not found.
-	if _, found := part.Notes[initial.Key()]; !found {
-		fmt.Println("Created initial value:...")
-		err := part.AddNote(initial)
-		fmt.Println("Add Note error:", err)
-	}
 
 	fmt.Printf("Got %d Notes and %d Voices\n", len(part.Notes), len(part.Voices))
 
