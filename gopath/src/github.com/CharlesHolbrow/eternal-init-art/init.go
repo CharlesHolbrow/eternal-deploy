05e3bf447930978b2d0bef5a4c6a8b6190ce08c3
Charles Holbrow <charlesholbrow@gmail.com>
2017-10-16 22:18:36 -0400

    Updating content

--- gopath/src/github.com/CharlesHolbrow/eternal-init-art/init.go
+++ gopath/src/github.com/CharlesHolbrow/eternal-init-art/init.go
@@ -54,21 +54,6 @@ func main() {
 	notes = append(notes, waysofthinking)
 	past.AddLink(waysofthinking.Key())
 
-	// Minsky
-	minsky := &eternal.Note{
-		Text: "Marvin Minsky",
-		ID:   "minsky",
-	}
-	notes = append(notes, minsky)
-	waysofthinking.AddLink(minsky.Key())
-
-	musicMindMeaning := &eternal.Note{
-		Text: "\"Why do we like music? Our culture immerses us in it for hours each day, and everyone knows how it touches our emotions, but few think of how music touches other kinds of thought. It is astonishing how little curiosity we have about so pervasive an \"environmental\" influence. What might we discover if we were to study musical thinking?",
-		ID:   "mmm",
-	}
-	notes = append(notes, musicMindMeaning)
-	minsky.AddLink(musicMindMeaning.Key())
-
 	// Murial Cooper
 	cooper := &eternal.Note{
 		Text: "Muriel Cooper",
@@ -78,11 +63,82 @@ func main() {
 	waysofthinking.AddLink(cooper.Key())
 
 	onCooper := &eternal.Note{
-		Text: "\"Muriel was a real pioneer of a new design domain,\" says Bill Mitchell, dean of MIT's School of Architecture and Planning. \"I think she was the first graphic designer to carry out really profound explorations of the new possibilities of electronic media—things like 3-D text. She didn't just see computer-graphics technology as a new tool for handling graphic design work. She understood from the beginning that the digital world opened up a whole domain of issues and problems, and she wanted to understand these problems in a rigorous way.\"",
+		Text: "\"Muriel was a real pioneer of a new design domain,\" said Bill Mitchell...",
 		ID:   "onmurial",
 	}
 	notes = append(notes, onCooper)
 	cooper.AddLink(onCooper.Key())
+
+	onCooper2 := &eternal.Note{
+		Text: "\"...I think she was the first graphic designer to carry out really profound explorations of the new possibilities of electronic media—things like 3-D text...\"",
+		ID:   "onmurial2",
+	}
+	notes = append(notes, onCooper2)
+	onCooper.AddLink(onCooper2.Key())
+
+	onCooper3 := &eternal.Note{
+		Text: "\"...She understood from the beginning that the digital world opened up a whole domain of issues and problems, and she wanted to understand these problems in a rigorous way.\"",
+		ID:   "onmurial3",
+	}
+	notes = append(notes, onCooper3)
+	onCooper2.AddLink(onCooper3.Key())
+
+	web := &eternal.Note{
+		Text: "Similarly, I think that the web opens a new world for digital media...",
+		ID:   "webworld",
+	}
+	notes = append(notes, web)
+	onCooper3.AddLink(web.Key())
+
+	notDiscovered := &eternal.Note{
+		Text: "...but that world has not yet been discovered.",
+		ID:   "notdiscovered",
+	}
+	notes = append(notes, notDiscovered)
+	web.AddLink(notDiscovered.Key())
+
+	// Minsky
+	minsky := &eternal.Note{
+		Text: "Marvin Minsky",
+		ID:   "minsky",
+	}
+	notes = append(notes, minsky)
+	waysofthinking.AddLink(minsky.Key())
+
+	musicMindMeaning := &eternal.Note{
+		Text: "Marvin asked: Why do we like music?...",
+		ID:   "mmm",
+	}
+	notes = append(notes, musicMindMeaning)
+	minsky.AddLink(musicMindMeaning.Key())
+
+	touches := &eternal.Note{
+		Text: "...Our culture immerses us in it for hours each day, and everyone knows how it touches our emotions",
+		ID:   "immerses",
+	}
+	notes = append(notes, touches)
+	musicMindMeaning.AddLink(touches.Key())
+
+	touchesThought := &eternal.Note{
+		Text: "...but few think of how music touches other kinds of thought...",
+		ID:   "touchesThought",
+	}
+	notes = append(notes, touchesThought)
+	touches.AddLink(touchesThought.Key())
+
+	pervasive := &eternal.Note{
+		Text: "...It is astonishing how little curiosity we have about so pervasive an \"environmental\" influence...",
+		ID:   "pervasive",
+	}
+	notes = append(notes, pervasive)
+	touchesThought.AddLink(pervasive.Key())
+
+	musicalThinking := &eternal.Note{
+		Text: "...What might we discover if we were to study musical thinking?",
+		ID:   "musicalthinking",
+	}
+	notes = append(notes, musicalThinking)
+	pervasive.AddLink(musicalThinking.Key())
 
 	////////////////////////////////////////////////////////////
 	//
@@ -103,24 +159,45 @@ func main() {
 	present.AddLink(pipeline.Key())
 
 	secret := &eternal.Note{
-		Text: "\"A lot of the art currently happening around the lab happens in secret. Students are so passionate about their work about that they do not tell their PIs for fear of being shut down. If there were some way to celebrate side projects or to support these efforts without detracting from students’ primary research, it could help our community to feel more like a family for those that are currently hiding.\"",
+		Text: "A lot of the art currently happening around the lab happens in secret.",
 		ID:   "secret",
 	}
 	notes = append(notes, secret)
 	present.AddLink(secret.Key())
 
+	secret2 := &eternal.Note{
+		Text: "Students are so passionate about their work about that they do not tell their PIs for fear of being shut down.",
+		ID:   "secret2",
+	}
+	notes = append(notes, secret2)
+	secret.AddLink(secret2.Key())
+
+	secret3 := &eternal.Note{
+		Text: "If there were some way to celebrate side projects or to support these efforts without detracting from students’ primary research, it could help our community to feel more like a family for those that are currently hiding.",
+		ID:   "secret3",
+	}
+	notes = append(notes, secret3)
+	secret2.AddLink(secret3.Key())
+
 	whatIs := &eternal.Note{
-		Text: "In hindsight, we can identify the projects and professors who shaped the art culture at the media lab. It's harder to tell what arts culture is right now. What do we want 'Art' to mean?",
+		Text: "In hindsight, we can identify the projects and professors who shaped the art culture at the media lab.",
 		ID:   "artmeaning",
 	}
 	notes = append(notes, whatIs)
 	present.AddLink(whatIs.Key())
 
+	harder := &eternal.Note{
+		Text: "It's harder to tell what arts culture is right now. What do we want 'Art' to mean?",
+		ID:   "harder",
+	}
+	notes = append(notes, harder)
+	whatIs.AddLink(harder.Key())
+
 	noMeaning := &eternal.Note{
 		Text: "The power of Art comes from its evasive definition.",
 		ID:   "evasive",
 	}
-	whatIs.AddLink(noMeaning.Key())
+	harder.AddLink(noMeaning.Key())
 	notes = append(notes, noMeaning)
 
 	paintbrush := &eternal.Note{
@@ -128,14 +205,14 @@ func main() {
 		ID:   "paintbrush",
 	}
 	notes = append(notes, paintbrush)
-	whatIs.AddLink(paintbrush.Key())
+	harder.AddLink(paintbrush.Key())
 
 	fineart := &eternal.Note{
 		Text: "Art is media that needs no explaination. Art opens our mind to new perspectives.",
 		ID:   "fineart",
 	}
 	notes = append(notes, fineart)
-	whatIs.AddLink(fineart.Key())
+	harder.AddLink(fineart.Key())
 
 	////////////////////////////////////////////////////////////
 	//
